1400329130ef489887ecf3f4f44b2e8474a4a569
Максим Хоцевич <xozewitc@yandex.ru>
2020-04-21 13:36:10 +0300

    Добавлено подключение к mongodb

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,22 +1,19 @@
 const express = require('express')
 const config = require('config')
-const firebase = require('firebase')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
-app.use(bodyParser.json())
 
 const PORT = config.get('PORT') || 5000
 
-app.get('/', function (req, res) {
-	console.log("HTTP Get Request");
-	res.send("HTTP GET Request");
-	firebase.database().ref('/TestMessages').set({ TestMessage: 'GET Request' });
-});
-
 async function start() {
 	try {
-		firebase.initializeApp(config.get('firebaseConfig'))
+		await mongoose.connect(
+			config.get('mongoURI'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
 		app.listen(PORT, () => console.log(`Server has been started on PORT ${PORT}...`))
 	} catch (e) {
 		console.log('Server error ', e.message)

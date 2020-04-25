18cad9f5ff8b980c23d77a1f4f1326d0c21f726a
Nelmeris <kufaevartem@icloud.com>
2020-04-25 12:34:45 +0400

    Создан адаптер для TableView
    Затраченных часов - 1,5

---
+++ src/iOS/UIViewKit/UIViewKit/TableViewAdapter/TableViewAdapter.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewAdapter.swift
+//  UIViewKit
+//
+//  Created by Artem Kufaev on 25.04.2020.
+//  Copyright © 2020 Artem Kufaev. All rights reserved.
+//
+
+import UIKit
+
+public class TableViewAdapter<Model, Cell: UITableViewCell>: NSObject,
+    UITableViewDelegate,
+    UITableViewDataSource
+    where Cell: ConfigurableCell, Model == Cell.ViewModel {
+    
+    private(set) weak var tableView: UITableView!
+    private(set) var viewModels: [Model] = [] {
+        didSet { tableView.reloadData() }
+    }
+    
+    public init(table: UITableView) {
+        self.tableView = table
+        super.init()
+        table.dataSource = self
+        table.delegate = self
+        table.register(Cell.self, forCellReuseIdentifier: Cell.reuseIdentifier)
+    }
+    
+    public func set(items: [Model]) {
+        self.viewModels = items
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: Cell = self.tableView.dequeueReusableCell(for: indexPath)
+        let viewModel = self.viewModels[indexPath.row]
+        cell.configure(viewModel, at: indexPath)
+        return cell
+    }
+    
+}

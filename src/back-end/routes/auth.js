97585c0c487ba68eb7db7ba145779e53b22264cf
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 16:30:27 +0300

    Сделал регистарцию, вход, выход

--- src/back-end/routes/auth.js
+++ src/back-end/routes/auth.js
@@ -1,38 +1,81 @@
 const { Router } = require('express')
-const { registerValidators } = require('../utils/validators')
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+
+const { registerValidators } = require('../utils/validators')
+const User = require('../models/User')
+
 const router = Router()
 
 // /api/auth/login
 router.post('/login', async (req, res) => {
 	try {
-		req.session.isAuthenticated = true
-		res.json(req.session)
+		const { email, password } = req.body
+		const candidate = await User.findOne({ email })
+
+		if (candidate) {
+			const areSame = await bcrypt.compare(password, candidate.password)
+			if (areSame) {
+				req.session.user = candidate
+				req.session.isAuthenticated = true
+				req.session.save(err => {
+					if (err) throw err
+					return res.status(200).json({
+						message: 'Успешно'
+					})
+				})
+			} else {
+				return res.status(400).json({
+					message: 'Неверный пароль'
+				})
+			}
+		} else {
+			return res.status(400).json({
+				message: 'Пользователь не найден'
+			})
+		}
 	} catch (e) {
 		console.log(e)
+		return res.status(500).json({
+			message: 'Server error'
+		})
 	}
 })
 
 // /api/auth/register
 router.post('/register', registerValidators, async (req, res) => {
 	try {
-		const { email, password, confirm } = req.body
+		const { email, password } = req.body
 
 		const errors = validationResult(req)
-		console.log(errors)
 		if (!errors.isEmpty()) {
-			// req.flash('registerError', errors.array()[0].msg)
-			return res.status(422)
+			return res.status(400).json({
+				message: errors.array()[0].msg
+			})
 		}
+		const hashedPassword = await bcrypt.hash(password, 10)
+		const user = new User({
+			email,
+			password: hashedPassword
+		})
+		await user.save()
+		return res.status(200).json({
+			message: 'Успешно'
+		})
 	} catch (e) {
 		console.log(e)
+		return res.status(500).json({
+			message: 'Server error'
+		})
 	}
 })
 
 // /api/auth/logout
 router.post('/logout', async (req, res) => {
 	req.session.destroy(() => {
-		res.json(req.session)
+		res.status(200).json({
+			message: 'Успешно'
+		})
 	})
 })
 

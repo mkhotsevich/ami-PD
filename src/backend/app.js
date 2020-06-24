3e7672b2c8997ef2646b03f111ce06029fc907b9
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 21:13:16 +0300

    Непонятная ошибка с гитом после переименования

---
+++ src/backend/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const compression = require('compression')
+const session = require('express-session')
+const morgan = require('morgan')
+const MongoStore = require('connect-mongodb-session')(session)
+
+const keys = require('./keys/index')
+const authRoutes = require('./routes/auth')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const store = new MongoStore({
+	collection: 'sessions',
+	uri: keys.MONGODB_URI
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.text())
+app.use(express.json({ type: 'application/json' }))
+
+app.use(session({
+	secret: keys.SECRET,
+	resave: false,
+	saveUninitialized: false,
+	store
+}))
+
+app.use(helmet())
+app.use(compression())
+
+app.use('/api/auth', authRoutes)
+
+if (process.env.NODE_ENV !== 'test') {
+	app.use(morgan('combined'));
+}
+
+async function start() {
+	try {
+		await mongoose.connect(keys.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		app.listen(PORT, () => console.log(`Server has been started on PORT ${PORT}...`))
+	} catch (e) {
+		console.log('Server error ', e.message)
+		process.exit(1)
+	}
+}
+
+start()
+ваыаыа
+module.exports = app
---
+++ src/backend/utils/validators.js
@@ -0,0 +1,30 @@
+const { body } = require('express-validator')
+const User = require('../models/User')
+
+exports.registerValidators = [
+	body('email', 'Некорректный email')
+		.isEmail()
+		.custom(async (value, {req}) => {
+			try {
+				const user = await User.findOne({ email: value })
+				if (user) {
+					return Promise.reject('Email занят')
+				}
+			} catch (e) {
+				console.log(e)
+			}
+		})
+		.normalizeEmail(),
+	body('password', 'Некорректный пароль')
+		.isLength({ min: 6, max: 64 })
+		.isAlphanumeric()
+		.trim(),
+	body('confirm')
+		.custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error('Пароли не совпадают')
+			}
+			return true
+		})
+		.trim()
+]

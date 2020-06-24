cccf0658983cbaaee93f4a5e7b2f59ef0093925f
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 14:48:27 +0300

    Создал пути для авторизации

--- src/back-end/routes/auth.js
+++ src/back-end/routes/auth.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const { registerValidators } = require('../utils/validators')
+const { validationResult } = require('express-validator')
 const router = Router()
 
 // /api/auth/login
 router.post('/login', async (req, res) => {
 	try {
-		console.log(req.body)
+		req.session.isAuthenticated = true
+		res.json(req.session)
 	} catch (e) {
 		console.log(e)
 	}
@@ -14,11 +16,24 @@ router.post('/login', async (req, res) => {
 // /api/auth/register
 router.post('/register', registerValidators, async (req, res) => {
 	try {
-		const {email, password, confirm} = req.body
-		console.log(email, password, confirm)
+		const { email, password, confirm } = req.body
+
+		const errors = validationResult(req)
+		console.log(errors)
+		if (!errors.isEmpty()) {
+			// req.flash('registerError', errors.array()[0].msg)
+			return res.status(422)
+		}
 	} catch (e) {
 		console.log(e)
 	}
+})
+
+// /api/auth/logout
+router.post('/logout', async (req, res) => {
+	req.session.destroy(() => {
+		res.json(req.session)
+	})
 })
 
 

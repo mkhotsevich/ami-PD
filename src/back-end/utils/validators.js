40d65bfb6910b6b287f1f8134f21e026967be9c3
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 14:20:57 +0300

    Добавил валидацию для email, password, confirn

--- src/back-end/utils/validators.js
+++ src/back-end/utils/validators.js
@@ -1,2 +1,26 @@
+const { body } = require('express-validator')
 
-exports.registerValidators = []
+exports.registerValidators = [
+	body('email', 'Некорректный email')
+		.isEmail()
+		.custom(async (value, {req}) => {
+			try {
+				// Проверка на существующий email
+			} catch (e) {
+				console.log(e)
+			}
+		})
+		.normalizeEmail(),
+	body('password', '/ПРИДУМАТЬ СООБЩЕНИЕ/')
+		.isLength({ min: 6, max: 64 })
+		.isAlphanumeric()
+		.trim(),
+	body('confirm')
+		.custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error('Пароли не совпадают')
+			}
+			return true
+		})
+		.trim()
+]

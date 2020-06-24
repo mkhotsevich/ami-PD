23b5119779dc213b8efb1c6638ca1ae4d34fbee8
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 19:35:11 +0300

    Переименовал папки

--- src/back-end/keys/keys.prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	MONGODB_URI: process.env.MONGODB_URI,
-	SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-	SECRET: process.env.SECRET
-}
--- src/back-end/keys/keys.test.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	MONGODB_URI: 'mongodb+srv://AMI_TEST:IvFo5OWUR3V9K3AZ@cluster0-4lwwx.mongodb.net/amidb_test',
-	SENDGRID_API_KEY: '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',
-	SECRET: 'BLACK LIVES NO MATTER'
-}

2c09874c0fa762b18a8a0d523d60ddcdac0f78db
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-24 18:45:23 +0300

    Тест для логина

--- src/back-end/test/auth.js
+++ src/back-end/test/auth.js
@@ -1,22 +1,24 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test'
 
-const mongoose = require("mongoose");
-const User = require('../models/User');
+const mongoose = require("mongoose")
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const bcrypt = require('bcryptjs')
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../app');
-const should = chai.should();
+const User = require('../models/User')
+const app = require('../app')
 
-chai.use(chaiHttp);
+const should = chai.should()
+
+chai.use(chaiHttp)
 describe('Users', () => {
 	beforeEach((done) => {
 		User.deleteMany({}, (err) => {
-			done();
-		});
-	});
-	describe('/POST User', () => {
-		it('Должна пройти регистрация без ошибок', (done) => {
+			done()
+		})
+	})
+	describe('/POST auth/register', () => {
+		it('Должна пройти регистрация с корректными данными', (done) => {
 			const user = {
 				email: 'xozewitc@yandex.ru',
 				password: '12345678',
@@ -27,9 +29,29 @@ describe('Users', () => {
 				.send(user)
 				.end((err, res) => {
 					res.should.have.status(200);
-					res.body.should.have.property('message');
-					done();
-				});
-		});
-	});
-});
+					done()
+				})
+		})
+	})
+	describe('/POST auth/login', () => {
+		it('Должен произойти вход с корректными данными', (done) => {
+			const user2 = {
+				email: 'maxim@yandex.ru',
+				password: '12345678'
+			}
+			const user = new User({
+				email: 'maxim@yandex.ru',
+				password: bcrypt.hashSync('12345678', 10)
+			})
+			user.save((err, user) => {
+				chai.request(app)
+					.post('/api/auth/login')
+					.send(user2)
+					.end((err, res) => {
+						res.should.have.status(200);
+						done()
+					})
+			})
+		})
+	})
+})

6adc5f8097bba0adb31c59b993e72d7787efc105
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-25 23:49:42 +0300

    Создал проверку токена

---
+++ src/backend/middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const keys = require('../keys/index')
+
+module.exports = (req, res, next) => {
+	if (req.method = 'OPTIONS') {
+		next()
+	}
+	try {
+		const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+		
+		if (!token) {
+			return res.status(401).json({message: 'Нет авторизации'})
+		}
+		const decoded = jwt.verify(token, keys.SECRET)
+		req.user = decoded
+		next()
+	} catch (e) {
+		res.status(401).json({ message: 'Нет авторизации' })
+	}
+}

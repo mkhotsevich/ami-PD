7f13a5148afe19ab50fef9bba79d788d2d8bad3e
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-25 11:29:36 +0300

    Сделал UI кнопку

---
+++ src/frontend/src/components/UI/Input/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Button.module.css'
+
+const Button = () => {
+	return (
+		<div className={classes.Button}>
+			<button>
+				Зарегистрироваться
+		</button>
+		</div>
+	)
+}
+
+export default Button
--- src/frontend/src/containers/Register/Register.js
+++ src/frontend/src/containers/Register/Register.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from 'react'
 import classes from './Register.module.css'
 import Input from '../../components/UI/Input/Input'
+import Button from '../../components/UI/Input/Button/Button'
 
 const Register = () => {
 	return (
 		<Fragment>
 			<div className={`row h-100`}>
 				<div className={'col-5 mx-auto d-flex flex-column justify-content-center h-100'}>
-					<div className={`${classes.Register} d-flex flex-column`}>
+					<div className={`${classes.Register} d-flex flex-column px-5`}>
 						<img />
 						<h1 className={'text-center'}>Регистрация</h1>
 						<Input/>
-						<Input/>
+						<Input />
+						<Button/>
 					</div>
 				</div>
 			</div>

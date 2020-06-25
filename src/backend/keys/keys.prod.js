e106b21f277b95609445c40d32fec7b1800031a5
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-26 01:39:14 +0300

    Добавил переменную

--- src/backend/keys/keys.prod.js
+++ src/backend/keys/keys.prod.js
@@ -1,5 +1,6 @@
 module.exports = {
 	MONGODB_URI: process.env.MONGODB_URI,
 	SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-	SECRET: process.env.SECRET
+	SECRET: process.env.SECRET,
+	HASH_SALT: process.env.HASH_SALT
 }

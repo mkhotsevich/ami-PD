905ad697422564612632303d1e283ec2699fef59
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-26 12:17:57 +0300

    Сверстал навигацию

--- src/frontend/src/components/Navbar/Navbar.js
+++ src/frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import logo from '../../images/logo.svg'
+import classes from './Navbar.module.css'
+import { Link, useHistory, NavLink } from 'react-router-dom'
+import AuthContext from '../../context/AuthContext'
 
 const Navbar = () => {
+	const history = useHistory()
+	const auth = useContext(AuthContext)
+
+	const logoutHandler = event => {
+		event.preventDefault()
+		auth.logout()
+		history.push('/')
+	}
+
 	return (
-		<div>
-			<h1>Navbar</h1>
+		<div className={classes.Navbar}>
+			<div className={'row'}>
+				<div className={'col-4 offset-4 d-flex justify-content-center align-items-center'}>
+					<img src={logo} alt={'logo'} />
+				</div>
+				<div className={'col-3 d-flex justify-content-end align-items-center'}>
+					<span>Здравствуйте, Йцуееккаукук</span>
+				</div>
+				<div className={'col-1 d-flex justify-content-end align-items-center'}>
+					<a href={'/'} onClick={logoutHandler}>Выйти</a>
+				</div>
+			</div>
+			<div className={'row'}>
+				<hr />
+			</div>
+			<div className={'row'}>
+				<div className={'col-4 text-center'}>
+					<NavLink to={'/'}>Здоровье</NavLink>
+				</div>
+				<div className={'col-4 text-center'}>
+					<NavLink to={'/'}>Статьи</NavLink>
+				</div>
+				<div className={'col-4 text-center'}>
+					<NavLink to={'/'}>Личная информация</NavLink>
+				</div>
+			</div>
 		</div>
 	)
 }

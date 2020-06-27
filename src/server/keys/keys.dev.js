469ada4e3e7e091fe4c36da5f95e646dc4ced35e
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-27 16:01:34 +0300

    Переименовал папку

--- src/server/keys/keys.dev.js
+++ src/server/keys/keys.dev.js
@@ -2,6 +2,6 @@ module.exports = {
 	MONGODB_URI: 'mongodb+srv://AMI:r0KZcEzNRMFJWdaV@cluster0-jgn8r.mongodb.net/amidb',
 	SENDGRID_API_KEY: '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',
 	SECRET: 'BLACK LIVES NO MATTER',
-	OUR_EMAIL: 'khotsevichmaxim@gmail.com', //TODO: придумать email
-	BASE_URL: '' //TODO: придумать 
+	OUR_EMAIL: 'info@amihp.ru',
+	BASE_URL: 'http://amihp.ru' //TODO: придумать 
 }

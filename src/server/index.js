380117aab4f1a939f7aeee03906245763201bcce
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-27 18:19:51 +0300

    Рефактор
    commit 9fb3bc1c8a585c67654faf7735d9c5f505d5a6c0
    Author: Maxim Khotsevich <xozewitc@yandex.ru>
    Date:   Sat Jun 27 18:19:31 2020 +0300
    
        Рефактор
    
    commit 0bc5c60aef9d3bfd5293bcfae4d46c794a11f3a6
    Author: Maxim Khotsevich <xozewitc@yandex.ru>
    Date:   Sat Jun 27 17:57:03 2020 +0300
    
        Добавил пакет для конфига
    
    commit 186c555f7c3e9edcd885d5fce7c5928b980493f9
    Author: Maxim Khotsevich <xozewitc@yandex.ru>
    Date:   Sat Jun 27 17:55:16 2020 +0300
    
        Пременил переименование
    
    commit d6be7b60aa4cb55ea0f7cb993adacea10c7e5344
    Author: Maxim Khotsevich <xozewitc@yandex.ru>
    Date:   Sat Jun 27 17:54:45 2020 +0300
    
        Переименовал главный файл
    
    commit 72ce3e0e1ae4b5af333bac17ae80c158b12ba4b4
    Author: Maxim Khotsevich <xozewitc@yandex.ru>
    Date:   Sat Jun 27 17:54:36 2020 +0300
    
        Удалили не нужные библиотеки

---
+++ src/server/index.js
@@ -0,0 +1,50 @@
+const compression = require('compression')
+const mongoose = require('mongoose')
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const config = require('config')
+const path = require('path')
+
+
+const app = express()
+const PORT = config.get('PORT')
+
+app.use(express.json({ extended: true }))
+app.use(helmet())
+app.use(compression())
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/users', require('./routes/users'))
+app.use('/api/history/water', require('./routes/water'))
+app.use('/api/history/sleep', require('./routes/sleep'))
+app.use('/api/history/weight', require('./routes/weight'))
+app.use('/api/history/tasks', require('./routes/tasks'))
+app.use('/api/articles', require('./routes/articles'))
+app.use('/api/token', require('./routes/token'))
+
+
+if (process.env.NODE_ENV !== 'test') app.use(morgan('combined'))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
+async function start() {
+	try {
+		await mongoose.connect(config.get('MONGODB_URI'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
+		app.listen(PORT, () => console.log(`Server has been started on PORT ${PORT}...`))
+	} catch (e) {
+		console.log('Неизвестная ошибка', e.message)
+		process.exit(1)
+	}
+}
+
+start()
+
+module.exports = app
--- src/server/keys/keys.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	PORT: 5000,
-	MONGODB_URI: 'mongodb+srv://AMI:r0KZcEzNRMFJWdaV@cluster0-jgn8r.mongodb.net/amidb',
-	SENDGRID_API_KEY: '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',
-	SECRET: 'BLACK LIVES NO MATTER',
-	OUR_EMAIL: 'info@amihp.ru',
-	BASE_URL: 'http://localhost:5000'
-}

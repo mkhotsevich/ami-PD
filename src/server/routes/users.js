7e82db205f0cf225cb388f17bda6f7a1e3a399f1
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-28 02:26:49 +0300

    Создал запросы

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -6,24 +6,63 @@ const router = Router()
 
 
 // /api/users
-router.get('/', auth, async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
 	try {
-		const users = await User.find()
-		res.status(200).json({ users })
-		console.log(req.user)
-	} catch (e) {
-		return res.status(500).json({
-			message: 'Server error'
+		const user = await User.findById(req.params.id, (err) => {
+			if (err) return res.status(404).json({ message: "Пользователь не найден" })
 		})
+
+		if (!user) return res.status(404).json({ message: "Пользователь не найден" })
+
+		return res.status(200).json({
+			id: user._id,
+			email: user.email,
+			name: user.name,
+			surname: user.surname,
+			birthdate: user.birthdate,
+			height: user.height,
+			appleId: user.appleId,
+			vkId: user.vkId
+		})
+	} catch (e) {
+		console.log(e)
+		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
 	}
 })
-router.get('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
 	try {
+		const { email, name, surname, birthdate, height, appleId, vkId } = req.body
+
+		const user = await User.findById(req.params.id, (err) => {
+			if (err) return res.status(404).json({ message: "Пользователь не найден" })
+		})
+
+		if (!user) return res.status(404).json({ message: "Пользователь не найден" })
+
+		user.email = email || user.email
+		user.name = name || user.name
+		user.surname = surname || user.surname
+		user.birthdate = birthdate || user.birthdate
+		user.height = height || user.height
+		user.appleId = appleId || user.appleId
+		user.vkId = vkId || user.vkId
+
+		await user.save()
+
+		return res.status(200).json({
+			id: user._id,
+			email: user.email,
+			name: user.name,
+			surname: user.surname,
+			birthdate: user.birthdate,
+			height: user.height,
+			appleId: user.appleId,
+			vkId: user.vkId
+		})
 
 	} catch (e) {
-		return res.status(500).json({
-			message: 'Server error'
-		})
+		console.log(e)
+		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
 	}
 })
 

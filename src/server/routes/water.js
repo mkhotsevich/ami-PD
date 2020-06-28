8858738d3055f7e1d81d27e46a9b29d1d18de275
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-29 00:58:59 +0300

    Доработал water

--- src/server/routes/water.js
+++ src/server/routes/water.js
@@ -6,17 +6,14 @@ const router = Router()
 
 router.get('/', auth, async (req, res) => {
 	try {
-		const waterHistory = await WaterHistory.find({ userId: req.user.userId })
-
-		res.status(200).json(
-			waterHistory
-		)
-
+		const waterHistory = await WaterHistory.find({ userId: req.user.userId }).select('_id amount drinkedAt')
+		res.status(200).json(waterHistory)
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
 	}
 })
+
 router.post('/', auth, async (req, res) => {
 	try {
 		const { amount, drinkedAt } = req.body
@@ -28,10 +25,10 @@ router.post('/', auth, async (req, res) => {
 		})
 		await waterHistory.save()
 
+		delete waterHistory._doc.userId
 		res.status(201).json({
-			waterHistory
+			...waterHistory._doc
 		})
-
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
@@ -42,9 +39,14 @@ router.put('/:id', auth, async (req, res) => {
 	try {
 		const { amount, drinkedAt } = req.body
 
-		const waterHistory = await WaterHistory.findById(req.params.id, (err) => {
+		const waterHistory = await WaterHistory.findOne({
+			_id: req.params.id,
+			userId: req.user.userId
+		}, (err) => {
 			if (err) return res.status(404).json({ message: 'Объект с данным ID не найден' })
-		})
+		}).select('amount drinkedAt')
+
+		if (!waterHistory) return res.status(404).json({ message: 'Объект с данным ID не найден' })
 
 		waterHistory.amount = amount || waterHistory.amount
 		waterHistory.drinkedAt = drinkedAt || waterHistory.drinkedAt
@@ -52,11 +54,8 @@ router.put('/:id', auth, async (req, res) => {
 		await waterHistory.save()
 
 		res.status(200).json({
-			id: waterHistory._id,
-			amount: waterHistory.amount,
-			drinkedAt: waterHistory.drinkedAt
+			...waterHistory._doc
 		})
-
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
@@ -70,15 +69,11 @@ router.delete('/:id', auth, async (req, res) => {
 			if (err) return res.status(404).json({ message: 'Объект с данным ID не найден' })
 		})
 
-		res.status(200).json({
-			message: "Успешно"
-		})
-
+		res.status(200).json({ message: "Успешно" })
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
 	}
 })
-
 
 module.exports = router

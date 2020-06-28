bd5c64ad7357652e665a12ba1be2e68c432f85e1
Nelmeris <kufaevartem@icloud.com>
2020-06-29 01:47:07 +0400

    Тестирование методов работы с данными сна
    Затрачено часов - 0,1
    Ожидание исправления багов на БЭКе

---
+++ src/iOS/DataManagerTests/SleepManagerTests.swift
@@ -0,0 +1,101 @@
+//
+//  SleepManagerTests.swift
+//  DataManager
+//
+//  Created by Artem Kufaev on 29.06.2020.
+//  Copyright © 2020 Artem Kufaev. All rights reserved.
+//
+
+import XCTest
+@testable import DataManager
+
+class SleepManagerTests: XCTestCase {
+    
+    lazy var manager: SleepManager = SleepManager()
+    
+    func testCreating() {
+        let exp = self.expectation(description: "Loading from server")
+        manager.save(endAt: Date(timeIntervalSinceNow: 36000),
+                     riseAt: Date()) { (result) in
+            switch result {
+            case .success(let data):
+                dump(data)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testGetting() {
+        let exp = self.expectation(description: "Loading from server")
+        var count = 0
+        manager.get { (result) in
+            switch result {
+            case .success(let data):
+                count += 1
+                dump(data)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            if count == 2 {
+                exp.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testUpdating() {
+        let exp = self.expectation(description: "Loading from server")
+        manager.get { (result) in
+            switch result {
+            case .success(let data):
+                guard let element = data.first else {
+                    XCTFail("Элемент для теста отсутствует")
+                    return
+                }
+                self.manager.update(id: element.id,
+                                    endAt: Date(timeIntervalSinceNow: 41539),
+                                    riseAt: Date(timeIntervalSinceNow: 10213)) { (result) in
+                    switch result {
+                    case .success(let data):
+                        dump(data)
+                    case .failure(let error):
+                        XCTFail(error.localizedDescription)
+                    }
+                    exp.fulfill()
+                }
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 20, handler: nil)
+    }
+    
+    func testDeleting() {
+        let exp = self.expectation(description: "Loading from server")
+        manager.get { (result) in
+            switch result {
+            case .success(let data):
+                guard let element = data.first else {
+                    XCTFail("Элемент для теста отсутствует")
+                    return
+                }
+                self.manager.delete(id: element.id) { (result) in
+                    switch result {
+                    case .success:
+                        dump("OK")
+                    case .failure(let error):
+                        XCTFail(error.localizedDescription)
+                    }
+                    exp.fulfill()
+                }
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 20, handler: nil)
+    }
+
+}

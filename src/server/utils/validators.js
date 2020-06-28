65678b6655f3bf7e12816ea41d50f0b96677987e
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-29 00:10:17 +0300

    Добавил валидаторы

--- src/server/utils/validators.js
+++ src/server/utils/validators.js
@@ -8,12 +8,16 @@ exports.registerValidators = [
 		.custom(async (value, { req }) => {
 			try {
 				const user = await User.findOne({ email: value })
-				if (user) {
-					return Promise.reject('Пользователь с таким Email уже существует')
-				}
+				if (user) return Promise.reject('Пользователь с таким Email уже существует')
 			} catch (e) {
 				console.log(e)
 			}
 		})
 		.normalizeEmail(),
+	body('password', 'Вы не указали пароль').exists(),
+	body('name', 'Вы не указали имя').exists(),
+	body('surname', 'Вы не указали фамилию').exists(),
+	body('birthdate', 'Вы не указали дату рождения').exists(),
+	body('weight', 'Вы не указали вес').exists(),
+	body('height', 'Вы не указали рост').exists(),
 ]

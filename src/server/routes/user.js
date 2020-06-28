56df45a4262effd745af2921860f8bb62350d457
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-29 00:41:47 +0300

    Доработал api/user

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -5,41 +5,31 @@ const auth = require('../middleware/auth.middleware')
 const router = Router()
 
 
-// /api/users
-router.get('/:id', auth, async (req, res) => {
+// /api/user
+router.get('/', auth, async (req, res) => {
 	try {
-		const user = await User.findById(req.params.id, (err) => {
-			if (err) return res.status(404).json({ message: "Пользователь не найден" })
-		})
-
+		const user = await User.findById(req.user.userId)
 		if (!user) return res.status(404).json({ message: "Пользователь не найден" })
 
+		delete user._doc.password
 		return res.status(200).json({
-			id: user._id,
-			email: user.email,
-			name: user.name,
-			surname: user.surname,
-			birthdate: user.birthdate,
-			height: user.height,
-			appleId: user.appleId,
-			vkId: user.vkId
+			...user._doc,
 		})
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })
 	}
 })
-router.put('/:id', auth, async (req, res) => {
+router.put('/', auth, async (req, res) => {
 	try {
-		const { email, name, surname, birthdate, height, appleId, vkId } = req.body
+		const { email, password, name, surname, birthdate, height, appleId, vkId } = req.body
 
-		const user = await User.findById(req.params.id, (err) => {
-			if (err) return res.status(404).json({ message: "Пользователь не найден" })
-		})
+		const user = await User.findById(req.user.userId)
 
 		if (!user) return res.status(404).json({ message: "Пользователь не найден" })
-		
+
 		user.email = email || user.email
+		user.password = password || user.password
 		user.name = name || user.name
 		user.surname = surname || user.surname
 		user.birthdate = birthdate || user.birthdate
@@ -49,17 +39,10 @@ router.put('/:id', auth, async (req, res) => {
 
 		await user.save()
 
+		delete user._doc.password
 		return res.status(200).json({
-			id: user._id,
-			email: user.email,
-			name: user.name,
-			surname: user.surname,
-			birthdate: user.birthdate,
-			height: user.height,
-			appleId: user.appleId,
-			vkId: user.vkId
+			...user._doc,
 		})
-
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({ message: 'Что-то пошло не так, попробуйте позже' })

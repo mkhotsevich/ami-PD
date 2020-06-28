133b5193a7670e2ddbdd3ee2892a563a87af9a98
Maxim Khotsevich <xozewitc@yandex.ru>
2020-06-28 14:57:33 +0300

    попытка номер 100

--- src/server/index.js
+++ src/server/index.js
@@ -12,19 +12,23 @@ const fs = require("fs");
 const app = express()
 const PORT = config.get('PORT')
 
+
+
+app.use(express.json({ extended: true, type: 'application/json' }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+
+
+
+app.use(helmet())
+app.use(compression())
+
 app.use(function (req, res, next) {
 	if ((req.get('X-Forwarded-Proto') !== 'https')) {
 		res.redirect('https://' + req.get('Host') + req.url)
 	} else
 		next()
 })
-
-app.use(express.json({ extended: true, type: 'application/json' }))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.text());
-
-app.use(helmet())
-app.use(compression())
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/users'))

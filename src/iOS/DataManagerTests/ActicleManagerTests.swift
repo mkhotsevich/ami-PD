24e43241f39ee1cde99f2374eb3f12a518102294
Nelmeris <kufaevartem@icloud.com>
2020-06-29 01:01:23 +0400

    Тестирование методов Article
    Затрачено часов - 0,05
    Ожидание заполнения данными базы

---
+++ src/iOS/DataManagerTests/ActicleManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ActicleManagerTests.swift
+//  DataManager
+//
+//  Created by Artem Kufaev on 29.06.2020.
+//  Copyright © 2020 Artem Kufaev. All rights reserved.
+//
+
+import XCTest
+@testable import DataManager
+
+class ArticleManagerTests: XCTestCase {
+    
+    lazy var manager: ArticleManager = ArticleManager()
+    
+    func testGetting() {
+        let exp = self.expectation(description: "Loading from server")
+        var count = 0
+        manager.get { (result) in
+            switch result {
+            case .success(let data):
+                count += 1
+                dump(data)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            if count == 2 {
+                exp.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
+}

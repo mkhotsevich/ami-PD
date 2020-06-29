5456b5e9e2be95bac6104912d8692d0bf94228b3
Nelmeris <kufaevartem@icloud.com>
2020-06-29 19:35:20 +0400

    Сверстан макет окна регистрации
    Затрачено часов - 0,75

--- src/iOS/ami/App/Application/AppDelegate.swift
+++ src/iOS/ami/App/Application/AppDelegate.swift
@@ -12,9 +12,9 @@ import DesignKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FontLib.printFonts()
         return true
     }
 

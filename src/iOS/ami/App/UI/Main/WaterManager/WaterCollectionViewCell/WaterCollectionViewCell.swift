9e74dbef94d0b49d0b4ca779d897cb71f9580f68
Nelmeris <kufaevartem@icloud.com>
2020-06-30 13:04:14 +0400

    Сверстано окно менеджера воды
    Затрачено часов - 2

---
+++ src/iOS/ami/App/UI/Main/WaterManager/WaterCollectionViewCell/WaterCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  WaterCollectionViewCell.swift
+//  ami
+//
+//  Created by Artem Kufaev on 30.06.2020.
+//  Copyright © 2020 Artem Kufaev. All rights reserved.
+//
+
+import UIKit
+
+class WaterCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var glassIcon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- src/iOS/ami/App/UI/Main/WaterManager/WaterManagerViewController.swift
+++ src/iOS/ami/App/UI/Main/WaterManager/WaterManagerViewController.swift
@@ -7,23 +7,102 @@
 //
 
 import UIKit
+import UIUtils
+import DataManager
 
 class WaterManagerViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var waterAmountLabel: UILabel!
+    
+    var waterHistory: [WaterInfo] = []
+    var weight: WeightInfo? {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    var glassCount: Int {
+        let weight = self.weight?.amount ?? 0
+        return Int(weight * 30 / 200)
+    }
+    var waterManager: WaterManager!
+    var weightManager: WeightManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCollectionView()
+        waterManager = WaterManager()
+        weightManager = WeightManager()
+        loadData()
+    }
+    
+    private func loadData() {
+        weightManager.get { (result) in
+            switch result {
+            case .success(let history):
+                self.weight = history.first
+            case .failure(let error):
+                fatalError(error.localizedDescription)
+            }
+        }
+        waterManager.get { (result) in
+            switch result {
+            case .success(let history):
+                let calendar = Calendar.current
+                self.waterHistory = history.filter { calendar.isDateInToday($0.drinkedAt) }
+            case .failure(let error):
+                fatalError(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func configureCollectionView() {
+        collectionView.register(UINib(nibName: "WaterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "WaterCellReusable")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension WaterManagerViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return GraphicHelper.calculateSizeOfSquaresInRectangle(rectangleSize: collectionView.frame.size, squareCount: glassCount)
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
 
+extension WaterManagerViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return glassCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WaterCellReusable",
+                                                            for: indexPath) as? WaterCollectionViewCell else { fatalError() }
+        if indexPath.row >= waterHistory.count,
+            let myImage = R.image.glassIcon() {
+            let tintableImage = myImage.withRenderingMode(.alwaysTemplate)
+            cell.glassIcon.image = tintableImage
+        }
+        return cell
+    }
+    
 }
